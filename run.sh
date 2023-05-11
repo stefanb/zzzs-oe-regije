915200e0dba139a03323e02c6ad0493ed0f87491
Štefan Baebler <stefan.baebler@gmail.com>
2023-05-11 19:35:03 +0200

    added sum POV_KM2

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ rm ZZZS_OE.geojson
 ogr2ogr ZZZS_OE.geojson OB.geojson -dialect sqlite \
  -sql "SELECT ST_Union(geometry),
 		sif.ZZZS_OE as ZZZS_OE,
-        GROUP_CONCAT(src.OB_UIME, ', ') as OBCINE
+        GROUP_CONCAT(src.OB_UIME, ', ') as OBCINE,
+        SUM(src.POV_KM2) as POV_KM2
 	FROM 'OB' AS src
 		LEFT JOIN 'obcina_zzzsoe_sif.csv'.obcina_zzzsoe_sif AS sif ON src.OB_UIME=sif.OB_UIME
 	GROUP BY sif.ZZZS_OE" \
  -explodecollections \
  -lco RFC7946=YES -lco WRITE_BBOX=YES \
  -nln ZZZS_OE
- 
