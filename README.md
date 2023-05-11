4bb9d10015ca4dc3ff6408bd1cdcecef3b2b0474
Štefan Baebler <stefan.baebler@gmail.com>
2023-05-11 18:56:17 +0200

    First working version, some name mismatches

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Občine: https://github.com/stefanb/gurs-rpe : [OB.geojson](https://github.com/stefanb/gurs-rpe/blob/master/data/OB.geojson)
+
+[ZZZS_OE.geojson](ZZZS_OE.geojson)
+
+Map: https://geojson.io/#id=github:stefanb/zzzs-oe-regije/blob/main/ZZZS_OE.geojson
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# curl -R https://raw.githubusercontent.com/stefanb/gurs-rpe/master/data/OB.geojson -o OB.geojson
+
+rm ZZZS_OE.geojson
+# http://gis.stackexchange.com/questions/85028/dissolve-aggregate-polygons-with-ogr2ogr-or-gpc
+ogr2ogr ZZZS_OE.geojson OB.geojson -dialect sqlite \
+ -sql "SELECT ST_Union(geometry),
+		sif.ZZZS_OE as ZZZS_OE,
+        GROUP_CONCAT(src.OB_UIME, ', ') as OBCINE
+	FROM 'OB' AS src
+		LEFT JOIN 'obcina_zzzsoe_sif.csv'.obcina_zzzsoe_sif AS sif ON src.OB_UIME=sif.OB_UIME
+	GROUP BY sif.ZZZS_OE" \
+ -explodecollections \
+ -lco RFC7946=YES -lco WRITE_BBOX=YES \
+ -nln ZZZS_OE
+ 
